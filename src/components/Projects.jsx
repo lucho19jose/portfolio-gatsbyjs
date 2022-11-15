dc08d26a75e03343768e6323f68d7de5c15badcd
lucho19jose <barbozagonzalesjose@gmail.com>
2022-11-14 21:32:54 -0500

    setting projects in the new card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,59 @@
 import React, {useState} from 'react'
+import { innovatec, zpedidos, diablo3 } from './innovatec'
 import './Projects.css'
 import './cards.css'
 
 const Projects = () => {
 
   const [Numcards, setNumcards] = useState([1,2,3, 4,5])
+
+  const projects = [
+    {
+      img: innovatec,
+      title: 'Siga CETPRO Perú',
+      description: 'This project is an academic system for manage processes like enrollment, grades, assistance in technical institutions. In this project my rol was as a Fullstack developer.',
+      tecnologies: ['Vue.js', 'Laravel', 'Quasar'],
+      urlgithub: '',
+      urllive: 'https://www.sigacetproperu.com/'
+    },
+    {
+      img: zpedidos,
+      title: 'Zpedidos web',
+      description: 'This is a delivery service where people can ask for food and services, I was as a fronted developer creating user interfaces.',
+      tecnologies: ['Vue.js', 'Bootstrap'],
+      urlgithub: '',
+      urllive: 'https://zpedidos.com/'
+    },
+    {
+      img: diablo3,
+      title: 'Profile Finder - Diablo3',
+      description: 'This is a personal project where people can look for profiles with BattleTag using Diablo 3 API.',
+      tecnologies: ['Vue.js', 'Bootstrap', 'Font Awesome'],
+      urlgithub: 'https://github.com/lucho19jose/Vue2-Diablo3Juego',
+      urllive: 'https://diablo3inproduction.netlify.app/#/'
+    },
+  ]
+
   return (
     <div className='projects-container'>
-          { Numcards.map((item) => ( 
-            <div className="img-card iCard-style1" key={item}>
+          { projects.map((item) => ( 
+            <div className="img-card iCard-style1" key={item.urllive}>
               <div className="card-content">
                   <div className="card-image">
                       {/* <span className="card-title">Cloud Beauty</span> */}
-                      <img src="../assets/imgs/zpedidos.png"/>
+                      <img src={item.img} alt='proyect in portfolio'/>
                   </div>
                   
                   <div className="card-text">
                       <p>
-                          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                          { item.description }
                       </p>
                   </div>
                   
               </div>
               
               <div className="card-link">
-                  <a href="https://www.sigacetproperu.com/" title="Read Full"><span>Read Full</span></a>
+                  <a href={item.urllive} title="See Live" target="_blank"><span>See Live</span></a>
               </div>
             </div>
           )) }
